ac6607b8291c50ceb453031838c228bb00814d82
Piratheepa <psatheeskumar01@student.rocvantwente.nl>
2018-11-14 16:38:00 +0100

    ...

--- Jansenautos/Models/EntitieAll.Designer.cs
+++ Jansenautos/Models/EntitieAll.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\User\Documents\GitHub\Jansengedoe\Jansenautos\Models\EntitieAll.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Piratheepa\Documents\Github\Jansengedoe\Jansenautos\Models\EntitieAll.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
